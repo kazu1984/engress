d03325f4191d88d4444ec568150a12fd9ddaf3b0
kazuto <0630zxcvbnm@gmail.com>
2022-05-10 05:34:54 +0900

    ご利用の流れセクションの作成

--- engress/front-page.php
+++ engress/front-page.php
@@ -32,6 +32,42 @@
     </div>
   </section>
 
+  <section class="l-flow p-flow">
+    <div class="l-inner">
+      <h2 class="c-section-title">ご利用の流れ</h2>
+      <ul class="p-flow__items">
+        <li class="p-flow__item">
+          <span class="p-flow__number">01</span>
+          <div class="p-flow__contents">
+            <h3 class="p-flow__title">お問い合わせ</h3>
+            <p class="p-flow__text">まずはフォームまたはお電話からお申し込みください。</p>
+          </div>
+        </li>
+        <li class="p-flow__item">
+          <span class="p-flow__number">02</span>
+          <div class="p-flow__contents">
+            <h3 class="p-flow__title">ヒアリング</h3>
+            <p class="p-flow__text">現在の学習状況やTOEFLスコア、目標のスコアをお聞きします。</p>
+          </div>
+        </li>
+        <li class="p-flow__item">
+          <span class="p-flow__number">03</span>
+          <div class="p-flow__contents">
+            <h3 class="p-flow__title">学習プランの提供</h3>
+            <p class="p-flow__text">目標達成のために最適な学習プランをご提案致します。</p>
+          </div>
+        </li>
+        <li class="p-flow__item">
+          <span class="p-flow__number">04</span>
+          <div class="p-flow__contents">
+            <h3 class="p-flow__title">入会</h3>
+            <p class="p-flow__text">お申込み完了後、レッスンがスタートします。</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+
   <div class="l-top-cta">
     <?php get_template_part('template-parts/template-cta', null); ?>
   </div>
