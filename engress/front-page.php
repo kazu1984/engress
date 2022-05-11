db74e18af01f07c085cbfa855700dbf4c5113f1e
kazuto <0630zxcvbnm@gmail.com>
2022-05-12 03:04:57 +0900

    3つの強みセクションの作成

--- engress/front-page.php
+++ engress/front-page.php
@@ -32,6 +32,44 @@
     </div>
   </section>
 
+  <section class="l-feature p-feature">
+    <div class="l-inner">
+      <h2 class="c-section-title">TOEFL対策に特化した<span>Engress3つの強み</span></h2>
+      <ul class="p-feature__items">
+        <li class="p-feature__item">
+          <div class="p-feature__content">
+            <span class="p-feature__label">特長 1</span>
+            <h3 class="p-feature__title">TOEFLに最適化された<br class="u-br-pc">無駄のないカリキュラム</h3>
+            <p class="p-feature__text">TOEFLではビジネス英語には登場しない数多くの学術的内容が出題されます。そのため、ベースとなる知識も必要になります。Engressでは過去1000題を分析し、最適なカリキュラムを組んでいます。</p>
+          </div>
+          <figure class="p-feature__img">
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/top/feature01.jpg" alt="engressの特徴画像">
+          </figure>
+        </li>
+        <li class="p-feature__item">
+          <div class="p-feature__content">
+            <span class="p-feature__label">特長 2</span>
+            <h3 class="p-feature__title">日本人指導歴10年以上の<br class="u-br-pc">経験豊富な講師陣</h3>
+            <p class="p-feature__text">Engressの講師陣は、もともと日本人向けにTOEFLを教えていた人が大多数です。また全メンバーがTESOL(英語教授法)を取得しており、知識と経験を兼ね備えている教育のプロフェッショナルです。</p>
+          </div>
+          <figure class="p-feature__img">
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/top/feature02.jpg" alt="engressの特徴画像">
+          </figure>
+        </li>
+        <li class="p-feature__item">
+          <div class="p-feature__content">
+            <span class="p-feature__label">特長 3</span>
+            <h3 class="p-feature__title">平均3ヶ月でTOEFL iBT20点アップ</h3>
+            <p class="p-feature__text">Engressは高校生からサラリーマンまで様々な年齢層の方々が通われていますが、完全オーダーメイドのカリキュラムで柔軟に対応しているため、平均3ヶ月でTOEFLスコアを20点アップさせています。</p>
+          </div>
+          <figure class="p-feature__img">
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/top/feature03.jpg" alt="engressの特徴画像">
+          </figure>
+        </li>
+      </ul>
+    </div>
+  </section>
+
   <section class="l-flow p-flow">
     <div class="l-inner">
       <h2 class="c-section-title">ご利用の流れ</h2>
@@ -88,7 +126,6 @@
           <h3 class="p-question__title js_accordion">TOEFL以外の海外大学合格のサポートもしてもらえるのでしょうか？</h3>
           <p class="p-question__answer js_answer">申し訳ございません。TOEFL以外のサポートは現在行っていません。申し訳ございません。TOEFL以外のサポートは現在行っていません。申し訳ございません。TOEFL</p>
         </li>
-
       </ul>
     </div>
   </section>
